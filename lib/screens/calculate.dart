def833776d117225169850dbf2539ad60ed9aed2
maleoaje <maleoaje@gmail.com>
2023-09-20 03:53:22 +0100

    calculate page done

--- lib/screens/calculate.dart
+++ lib/screens/calculate.dart
@@ -1,3 +1,5 @@
+import 'dart:developer';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_animate/flutter_animate.dart';
 import 'package:move_mate/constants/constants.dart';
@@ -10,7 +12,8 @@ class CalculatePage extends StatefulWidget {
 }
 
 class _CalculatePageState extends State<CalculatePage> {
-  final int _select = 0;
+  bool _select = false;
+  int _chooseCat = 0;
   final TextEditingController _senderLoc = TextEditingController();
   final TextEditingController _weight = TextEditingController();
   final TextEditingController _receiverLoc = TextEditingController();
@@ -271,7 +274,7 @@ class _CalculatePageState extends State<CalculatePage> {
                                     cursorColor: primaryColor,
                                     textAlign: TextAlign.start,
                                     keyboardType: TextInputType.name,
-                                    controller: _senderLoc,
+                                    controller: _receiverLoc,
                                     style: const TextStyle(),
                                     decoration: InputDecoration(
                                       contentPadding: const EdgeInsets.fromLTRB(
@@ -364,7 +367,7 @@ class _CalculatePageState extends State<CalculatePage> {
                                     cursorColor: primaryColor,
                                     textAlign: TextAlign.start,
                                     keyboardType: TextInputType.name,
-                                    controller: _senderLoc,
+                                    controller: _weight,
                                     style: const TextStyle(),
                                     decoration: InputDecoration(
                                       contentPadding: const EdgeInsets.fromLTRB(
@@ -515,7 +518,345 @@ class _CalculatePageState extends State<CalculatePage> {
                 ],
               ),
             ),
+            Animate(
+              effects: const [
+                MoveEffect(
+                  begin: Offset(0, 00),
+                  end: Offset(0, -70),
+                  duration: Duration(milliseconds: 600),
+                ),
+                FadeEffect(
+                  begin: 0,
+                  delay: Duration(milliseconds: 300),
+                  duration: Duration(
+                    milliseconds: 300,
+                  ),
+                )
+              ],
+              child: Padding(
+                padding: const EdgeInsets.all(20.0),
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text(
+                      'Categories',
+                      style: TextStyle(
+                          color: black,
+                          fontSize: 18,
+                          fontWeight: FontWeight.w600),
+                    ),
+                    Text(
+                      'What are you sending?',
+                      style: TextStyle(
+                          color: black.withOpacity(.5),
+                          fontSize: 16,
+                          fontWeight: FontWeight.w400),
+                    ),
+                    const SizedBox(
+                      height: 20,
+                    ),
+                    Animate(
+                      effects: const [
+                        MoveEffect(
+                            begin: Offset(100, 0),
+                            end: Offset(0, 0),
+                            duration: Duration(milliseconds: 600)),
+                        FadeEffect(
+                          begin: .0,
+                          duration: Duration(
+                            milliseconds: 700,
+                          ),
+                        )
+                      ],
+                      child: Row(
+                        children: [
+                          Animate(
+                            effects: const [
+                              MoveEffect(
+                                  begin: Offset(100, 0),
+                                  delay: Duration(milliseconds: 100),
+                                  end: Offset(0, 0),
+                                  duration: Duration(milliseconds: 600)),
+                              FadeEffect(
+                                begin: .0,
+                                duration: Duration(
+                                  milliseconds: 700,
+                                ),
+                              )
+                            ],
+                            child: categoryCards(
+                              'Documents',
+                              1,
+                              95,
+                            ),
+                          ),
+                          Animate(
+                            effects: const [
+                              MoveEffect(
+                                begin: Offset(100, 0),
+                                end: Offset(0, 0),
+                                duration: Duration(milliseconds: 600),
+                                delay: Duration(milliseconds: 200),
+                              ),
+                              FadeEffect(
+                                begin: .0,
+                                duration: Duration(
+                                  milliseconds: 700,
+                                ),
+                              ),
+                            ],
+                            child: categoryCards(
+                              'Glass',
+                              2,
+                              60,
+                            ),
+                          ),
+                          Animate(
+                            effects: const [
+                              MoveEffect(
+                                begin: Offset(100, 0),
+                                end: Offset(0, 0),
+                                duration: Duration(milliseconds: 600),
+                                delay: Duration(milliseconds: 300),
+                              ),
+                              FadeEffect(
+                                begin: .0,
+                                duration: Duration(
+                                  milliseconds: 700,
+                                ),
+                              ),
+                            ],
+                            child: categoryCards(
+                              'Liquid',
+                              3,
+                              65,
+                            ),
+                          ),
+                          Animate(
+                              effects: const [
+                                MoveEffect(
+                                  begin: Offset(100, 0),
+                                  end: Offset(0, 0),
+                                  duration: Duration(milliseconds: 600),
+                                  delay: Duration(milliseconds: 400),
+                                ),
+                                FadeEffect(
+                                  begin: .0,
+                                  duration: Duration(
+                                    milliseconds: 700,
+                                  ),
+                                ),
+                              ],
+                              child: categoryCards(
+                                'Food',
+                                4,
+                                55,
+                              )),
+                        ],
+                      ),
+                    ),
+                    const SizedBox(
+                      height: 10,
+                    ),
+                    Animate(
+                      effects: const [
+                        MoveEffect(
+                            begin: Offset(100, 0),
+                            end: Offset(0, 0),
+                            duration: Duration(milliseconds: 600)),
+                        FadeEffect(
+                          begin: .0,
+                          duration: Duration(
+                            milliseconds: 700,
+                          ),
+                        )
+                      ],
+                      child: Row(
+                        children: [
+                          Animate(
+                              effects: const [
+                                MoveEffect(
+                                    begin: Offset(100, 0),
+                                    delay: Duration(milliseconds: 100),
+                                    end: Offset(0, 0),
+                                    duration: Duration(milliseconds: 600)),
+                                FadeEffect(
+                                  begin: .0,
+                                  duration: Duration(
+                                    milliseconds: 700,
+                                  ),
+                                )
+                              ],
+                              child: categoryCards(
+                                'Electronic',
+                                5,
+                                90,
+                              )),
+                          Animate(
+                              effects: const [
+                                MoveEffect(
+                                  begin: Offset(100, 0),
+                                  end: Offset(0, 0),
+                                  duration: Duration(milliseconds: 600),
+                                  delay: Duration(milliseconds: 200),
+                                ),
+                                FadeEffect(
+                                  begin: .0,
+                                  duration: Duration(
+                                    milliseconds: 700,
+                                  ),
+                                ),
+                              ],
+                              child: categoryCards(
+                                'Product',
+                                6,
+                                75,
+                              )),
+                          Animate(
+                              effects: const [
+                                MoveEffect(
+                                  begin: Offset(100, 0),
+                                  end: Offset(0, 0),
+                                  duration: Duration(milliseconds: 600),
+                                  delay: Duration(milliseconds: 300),
+                                ),
+                                FadeEffect(
+                                  begin: .0,
+                                  duration: Duration(
+                                    milliseconds: 700,
+                                  ),
+                                ),
+                              ],
+                              child: categoryCards(
+                                'Others',
+                                7,
+                                69,
+                              )),
+                        ],
+                      ),
+                    )
+                  ],
+                ),
+              ),
+            ),
           ],
+        ),
+      ),
+      bottomNavigationBar: Padding(
+        padding: const EdgeInsets.fromLTRB(20, 0, 20, 10),
+        child: SizedBox(
+          height: 64,
+          child: InkWell(
+            highlightColor: white,
+            splashColor: Colors.transparent,
+            onTap: () {
+              setState(() {
+                _select = !_select;
+              });
+              log('here');
+            },
+            child: _select
+                ? Animate(
+                    effects: const [
+                      MoveEffect(
+                        begin: Offset(4, 1),
+                        end: Offset(0, 0),
+                      )
+                    ],
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 5),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: primaryOrange,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: const Center(
+                        child: Text(
+                          'Calculate',
+                          style: TextStyle(
+                              color: white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  )
+                : Animate(
+                    effects: const [
+                      MoveEffect(
+                          begin: Offset(4, 1),
+                          end: Offset(0, 0),
+                          delay: Duration(milliseconds: 1))
+                    ],
+                    child: Container(
+                      padding: const EdgeInsets.symmetric(
+                          horizontal: 10, vertical: 4),
+                      width: MediaQuery.of(context).size.width,
+                      decoration: BoxDecoration(
+                          color: primaryOrange,
+                          borderRadius: BorderRadius.circular(50)),
+                      child: const Center(
+                        child: Text(
+                          'Calculate',
+                          style: TextStyle(
+                              color: white,
+                              fontSize: 16,
+                              fontWeight: FontWeight.w500),
+                        ),
+                      ),
+                    ),
+                  ),
+          ),
+        ),
+      ),
+    );
+  }
+
+  Widget categoryCards(title, catInt, double wwid) {
+    return InkWell(
+      splashColor: transparent,
+      highlightColor: transparent,
+      onTap: () {
+        setState(() {
+          _chooseCat = catInt;
+        });
+        log(_chooseCat.toString());
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(right: 10),
+        child: AnimatedContainer(
+          width: _chooseCat == catInt ? wwid + 30 : wwid,
+          duration: const Duration(milliseconds: 400),
+          padding: const EdgeInsets.all(10),
+          decoration: BoxDecoration(
+              color: _chooseCat == catInt ? black : transparent,
+              borderRadius: BorderRadius.circular(10),
+              border: Border.all(width: 1.5, color: primarygreyDark)),
+          child: Row(
+            children: [
+              _chooseCat == catInt
+                  ? const Row(
+                      children: [
+                        Icon(
+                          Icons.check,
+                          color: white,
+                          size: 15,
+                        ),
+                        SizedBox(
+                          width: 8,
+                        ),
+                      ],
+                    )
+                  : Container(),
+              Text(
+                title,
+                style: TextStyle(
+                    color: _chooseCat == catInt ? white : black,
+                    fontSize: 14,
+                    fontWeight: FontWeight.w400),
+              ),
+            ],
+          ),
         ),
       ),
     );
